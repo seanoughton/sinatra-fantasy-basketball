88d33340ffe0c943454215480887132de2063d75
Sean Oughton <seanoughton@gmail.com>
2018-04-11 10:53:45 -0700

    Create Basic classes

---
+++ app/models/team.rb
@@ -0,0 +1,7 @@
+
+class Team < ActiveRecord::Base
+
+	belongs_to :user
+	has_many :players
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+
+	has_secure_password
+	has_many :teams
+	has_many :players through: :teams
+
+end
